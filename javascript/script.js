fd80e76bf5b0f8cba7ad10d9f001e0be490a501b
MIJINYAWA664 <mijinyawadau664@gmail.com>
2025-06-26 14:52:04 +0100

    array/object

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+var user = {
+    username: "ada_lovelace",
+    password: "secure123"
+  };
+  
+ var database = [user];
+  
+ var newsfeed = [
+    {
+      username: "john_doe",
+      timeline: "Just finished my JS project!"
+    },
+    {
+      username: "mary_jane",
+      timeline: "Loving the new React update 😍"
+    },
+    {
+      username: "tech_guru",
+      timeline: "Debugged for 5 hours... it was a semicolon."
+    }
+  ];
+  
+  
