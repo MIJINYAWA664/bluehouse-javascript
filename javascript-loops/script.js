7b307d402353b1935d76b3fcdb6a6c7f06456218
MIJINYAWA664 <mijinyawadau664@gmail.com>
2025-07-08 12:24:36 +0100

    done

---
+++ javascript-loops/script.js
@@ -0,0 +1,15 @@
+for (var i = 1; i < 10; i++){
+    if (i % 2 === 0)
+        console.log(i);
+}
+
+var count = 10
+while (count >= 1){
+    console.log(count);
+    count--
+}
+
+var number = 5
+do {
+    console.log(number);
+} while (number < 2);
