62569702871963af0abfe5df4e05d3fdb81acadd
MIJINYAWA664 <mijinyawadau664@gmail.com>
2025-07-17 17:16:22 +0100

    trial 1

---
+++ javascript-practice/guessing game.js
@@ -0,0 +1,11 @@
+var digit = prompt("enter a random number from 0-10: ")
+digit = Number(digit)
+var random = Math.floor(Math.random() * (10)) + 1;
+while (digit !== random) {
+    alert("try again")
+    digit = prompt("enter a random number from 0-10: ")
+    digit = Number(digit)
+    random = Math.floor(Math.random() * (10)) + 1;
+    console.log(random)
+}
+alert("correct");
